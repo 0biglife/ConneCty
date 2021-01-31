b90137d108770adf5fbcec266dc1286f262f6bef
0biglife <kimchoon42@gmail.com>
2021-01-31 21:08:40 +0900

    Initial Commit

---
+++ connecty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  connecty
+//
+//  Created by 공대생 on 2021/01/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
